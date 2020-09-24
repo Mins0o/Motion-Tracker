d4291c5271a00c10c543835d83a3a6b72bb41b4b
MINSOO KANG <stengine2@gmail.com>
2020-09-24 13:15:25 +0900

    Naive Convolution

--- Convolution.py
+++ Convolution.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def naive_convolve(target, kernel):
-
 	size_x, size_y = target.shape[:2]
 	kernel_x, kernel_y = kernel.shape[:2]
 	# target shape validi_Aty check
 	
 	
-	convolved_image = np.zeros((size_x - kernel_x + 1,size_y - kernel_y + 1, 3))
+	convolved_image = np.zeros((size_x - kernel_x + 1,size_y - kernel_y + 1, 3), dtype = 'int32')
 	# one channel
 	#if target.shape[2] == 1:
+	target = np.average(target,2)
 	for x in range(kernel_x // 2, size_x - kernel_x // 2):
 		for y in range(kernel_y // 2, size_y - kernel_y // 2):
 			target_window = target[x - kernel_x // 2 : x + kernel_x // 2 + 1, y - kernel_y // 2 : y + kernel_y //2 + 1]
@@ -18,6 +18,7 @@ def naive_convolve(target, kernel):
 	
 	plt.imshow(convolved_image,'gray')
 	plt.show()
+	return convolved_image
 	
 	if target.shape[2] == 3:
 		# separate channels
--- Image Loader.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-def numpy_image(path):
-	img = Image.open(path)
-	return np.array(img)

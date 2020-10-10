0f56394ba8fd5d7eb5983d03459ba2df93abc82b
MINSOO KANG <stengine2@gmail.com>
2020-10-10 23:58:26 +0900

    making exponent matrix

--- FourierTransform.py
+++ FourierTransform.py
@@ -2,4 +2,10 @@ import numpy as np
 
 
 def ft_2d_naive(image):
-	pass
+	image_x, image_y = image.shape[:2]
+	x_sum = np.arange(-image_x // 2, image_x // 2)[:,np.newaxis]
+	x_exponent = (x_sum[:,np.newaxis] * x_sum) / image_x
+	y_sum = np.arange(-image_y // 2, image_y // 2)[:,np.newaxis]
+	y_exponent = (y_sum[:,np.newaxis] * y_sum) / image_y
+	exp_matrix = np.exp(-2 * np.pi * (0+1j) * (x_exponent[:,:,np.newaxis,np.newaxis] + y_exponent))
+	return(exp_matrix)

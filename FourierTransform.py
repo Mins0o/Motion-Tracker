3a5b3daf1e3debe98b725c7efafa23d77709be3f
MINSOO KANG <stengine2@gmail.com>
2020-10-12 21:45:29 +0900

    Fixed the naive 2D Fourier transform

--- FourierTransform.py
+++ FourierTransform.py
@@ -3,10 +3,12 @@ import numpy as np
 
 def ft_2d_naive(image):
 	image_x, image_y = image.shape[:2]
-	x_sum = np.arange(-image_x // 2, image_x // 2)[:,np.newaxis]
-	x_exponent = (x_sum[:,np.newaxis] * x_sum) / image_x
-	y_sum = np.arange(-image_y // 2, image_y // 2)[:,np.newaxis]
-	y_exponent = (y_sum[:,np.newaxis] * y_sum) / image_y
-	exp_matrix = np.exp(-2 * np.pi * (0+1j) * (x_exponent[:,:,np.newaxis,np.newaxis] + y_exponent))
-	ft_matrix = np.sum(np.sum(exp_matrix,axis=4),axis=3)
+	x_sum = np.arange(image_x) - (image_x // 2)
+	y_sum = np.arange(image_y) - (image_y // 2)
+	ft_matrix = np.zeros((image_x, image_y),np.complex64)
+	
+	for u in x_sum:
+		for v in y_sum:
+			exponent = -2 * np.pi * (0+1j) * ((u * x_sum / image_x)[:, np.newaxis] + (v * y_sum/image_y))
+			ft_matrix[u,v] = (image*np.exp(exponent)).sum()
 	return(ft_matrix)

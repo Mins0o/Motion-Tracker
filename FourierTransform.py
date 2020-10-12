5c8aace39f5315605f69861e6d623fab0c898791
MINSOO KANG <stengine2@gmail.com>
2020-10-13 01:20:26 +0900

    a bit faster naive fourier transform

--- FourierTransform.py
+++ FourierTransform.py
@@ -8,7 +8,15 @@ def ft_2d_naive(image):
 	ft_matrix = np.zeros((image_x, image_y),np.complex64)
 	
 	for u in x_sum:
-		for v in y_sum:
-			exponent = -2 * np.pi * (0+1j) * ((u * x_sum / image_x)[:, np.newaxis] + (v * y_sum/image_y))
-			ft_matrix[u,v] = (image*np.exp(exponent)).sum()
+		exponent = -2 * np.pi * (0+1j) * ((u * x_sum / image_x)[:, np.newaxis, np.newaxis] + (y_sum[:,np.newaxis] * y_sum/image_y))
+		print(exponent.shape)
+		#for v in y_sum:
+		#	exponent = -2 * np.pi * (0+1j) * ((u * x_sum / image_x)[:, np.newaxis] + (v * y_sum/image_y))
+		#	ft_matrix[u,v] = (image*np.exp(exponent)).sum()
+		ft_matrix[u] = (image[:,np.newaxis] * np.exp(exponent)).sum(axis = 1).sum(axis = 0)
+		print("{0} / {1}".format(u + 1, image_x // 2), end = "\r")
 	return(ft_matrix)
+	
+def display_fourier(fourier):
+	plt.imshow(np.log(np.abs(np.fft.fftshift(fourier))),'gray')
+	

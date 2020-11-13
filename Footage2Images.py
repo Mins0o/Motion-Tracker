aeb3b955b4e9f8fd81f3ff2be9673cb296ad7d04
MINSOO KANG <stengine2@gmail.com>
2020-11-13 23:58:15 +0900

    Update Footage2Images.py

--- Footage2Images.py
+++ Footage2Images.py
@@ -3,7 +3,16 @@ import cv2
 # This module is to apply the template matching in a video
 
 def fetch_images(footage, start, steps, end):
-
+	frames = []
+	for i in range(start):
+		footage.grab()
+	for i in range(end - start):
+		if i % steps == 0:
+			frames.append(footage.read())
+		else:
+			footage.grab()
+	return frames
+			
 video = cv2.VideoCapture("")
 
 video.read()
